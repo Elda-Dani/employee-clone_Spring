b15796a03f8b89221946a72aaa02efbe34bcf553
Elda-Dani <eldadani2001@gmail.com>
2022-10-16 11:01:58 +0530

    emp

---
+++ src/main/java/com/nestdigital/employeeAppclone/EmployeeAppCloneApplication.java
@@ -0,0 +1,13 @@
+package com.nestdigital.employeeAppclone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeAppCloneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeAppCloneApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nestdigital/employeeAppclone/EmployeeAppCloneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nestdigital.employeeAppclone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeAppCloneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
